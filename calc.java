6aa4c8aa8fff1da8404bc3b6b8b5393f90c25975
Manalang <gamingpurposes019@gmail.com>
2024-09-10 11:19:23 +0300

    initial commit of main calculator file

---
+++ calc.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class calc {
+    public static void main(String[]args){
+    
+
+    Scanner scanner = new Scanner(System.in);
+
+    System.out.println("Enter the first number: ");
+    double num1 = scanner.nextDouble();
+
+    System.out.println("Enter the second number: ");
+    double num2 = scanner.nextDouble();
+
+    System.out.println("Sum: " + addition(num1, num2));
+    System.out.println("Difference: " + subtraction(num1, num2));
+    System.out.println("Product: " + multiplication(num1, num2));
+    System.out.println("Quotient: " + division(num1, num2));
+    System.out.println("Modulus: " + modulus(num1, num2));
+
+
+    } 
+    //Methods Here
+}
+
